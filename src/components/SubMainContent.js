04aea89f0c38be3a33dacb055594fa8e004f72b7
Paul Choi <paulsoonminchoi@gmail.com>
2023-11-16 14:53:34 -0800

    Fix button sizing and remove sa project

--- src/components/SubMainContent.js
+++ src/components/SubMainContent.js
@@ -11,56 +11,101 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { twitch1, twitch2, twitch3, twitch4, twitch5, twitch6 } from "../public/twitch";
-import { mtndew1, mtndew2, mtndew3, mtndew4, mtndew5, mtndew6 } from "../public/mtndew";
-import { mediaone1, mediaone2, mediaone3, mediaone4, mediaone5, mediaone6 } from "../public/mediaone";
-import { waveform1, waveform2, waveform3, waveform4, waveform5, waveform6 } from "../public/waveform";
-
+import {
+  twitch1,
+  twitch2,
+  twitch3,
+  twitch4,
+  twitch5,
+  twitch6,
+} from "../public/twitch";
+import {
+  mtndew1,
+  mtndew2,
+  mtndew3,
+  mtndew4,
+  mtndew5,
+  mtndew6,
+} from "../public/mtndew";
+import {
+  mediaone1,
+  mediaone2,
+  mediaone3,
+  mediaone4,
+  mediaone5,
+  mediaone6,
+} from "../public/mediaone";
+import {
+  waveform1,
+  waveform2,
+  waveform3,
+  waveform4,
+  waveform5,
+  waveform6,
+} from "../public/waveform";
 
 // Import our Components
 import HorizontalScroll from "./horizontalScroll";
 
 const SubMainContent = () => {
   const [nav, setNav] = useState("About");
-  const [project, setProject] = useState('');
-  
-  
+  const [project, setProject] = useState("");
+
   const imgObj = {
     1: pic1,
     2: pic2,
     3: pic3,
     4: pic4,
-    5: pic5,
+    // 5: pic5,
   };
-  
+
   const projectNames = {
-    1: ['Twitch', 'Software Engineer / Frontend'],
-    2: ['Mtndew', 'Freelance / Web Development'],
-    3: ['Media One', 'Freelance / Web Development'],
-    4: ['Waveform', 'Software Engineer / Frontend'],
-    5: ['Soul Assassins', 'Freelance / Web Development'],
+    1: ["Twitch", "Software Engineer / Frontend"],
+    2: ["Mtndew", "Freelance / Web Development"],
+    3: ["Media One", "Freelance / Web Development"],
+    4: ["Waveform", "Software Engineer / Frontend"],
+    // 5: ["Soul Assassins", "Freelance / Web Development"],
   };
 
   const subPhotos = {
-    'Twitch': [twitch1, twitch2, twitch3, twitch4, twitch5, twitch6],
-    'Mtndew': [mtndew1, mtndew2, mtndew3, mtndew4, mtndew5, mtndew6],
-    'Media One': [mediaone1, mediaone2, mediaone3, mediaone4, mediaone5, mediaone6],
-    'Waveform': [waveform1, waveform2, waveform3, waveform4, waveform5, waveform6],
-    'Soul Assassins': [twitch1, twitch2, twitch3, twitch4, twitch5, twitch6]
-  }
+    Twitch: [twitch1, twitch2, twitch3, twitch4, twitch5, twitch6],
+    Mtndew: [mtndew1, mtndew2, mtndew3, mtndew4, mtndew5, mtndew6],
+    "Media One": [
+      mediaone1,
+      mediaone2,
+      mediaone3,
+      mediaone4,
+      mediaone5,
+      mediaone6,
+    ],
+    Waveform: [
+      waveform1,
+      waveform2,
+      waveform3,
+      waveform4,
+      waveform5,
+      waveform6,
+    ],
+    // "Soul Assassins": [twitch1, twitch2, twitch3, twitch4, twitch5, twitch6],
+  };
 
   const SampleCards = React.memo(() =>
-    Array(5)
+    Array(4)
       .fill(0)
       .map((_e, i) => (
         <>
           <div
             key={`sampleCard-${i}`}
-            className={`project-card relative ${project === projectNames[i + 1][0] ? "h-[500px] w-[1200px]" : "h-[340px] w-[540px]"} mr-[70px] shrink-0`}>
-            <div 
-              className={`img-box ${project === projectNames[i + 1][0] ? "hide-project" : ""}`} 
-              onClick={() => setProject(projectNames[i + 1][0])}
-            >
+            className={`project-card relative ${
+              project === projectNames[i + 1][0]
+                ? "h-[500px] w-[1200px]"
+                : "h-[340px] w-[540px]"
+            } mr-[70px] shrink-0`}>
+            <div
+              className={`img-box ${
+                project === projectNames[i + 1][0] ? "hide-project" : ""
+              }`}
+              onClick={() => setProject(projectNames[i + 1][0])}>
               <img
                 className="projects"
                 alt="test"
@@ -72,20 +117,33 @@ const SubMainContent = () => {
                   <p>{projectNames[i + 1][0]}</p>
                   <p className="opacity-low">{projectNames[i + 1][1]}</p>
                 </div>
-              </div> 
+              </div>
             </div>
             {/* SHOW SUB PROJECT GALLERY PHOTOS */}
-            <div  className="project-sub-photos">
+            <div className="project-sub-photos">
               <div className="gallery">
                 {subPhotos[projectNames[i + 1][0]].map((photo, j) => {
                   return (
-                    <img className={`${project === projectNames[i + 1][0] ? `animate glow delay-${j + 1}` : ""}`} src={photo} alt={`${project}-${j}`} />
-                  )
+                    <img
+                      className={`${
+                        project === projectNames[i + 1][0]
+                          ? `animate glow delay-${j + 1}`
+                          : ""
+                      }`}
+                      src={photo}
+                      alt={`${project}-${j}`}
+                    />
+                  );
                 })}
-                
               </div>
             </div>
-            <div className={`${project === projectNames[i + 1][0] ? "project-sub-photos-overlay" : ""}`} onClick={() => setProject('')}></div>
+            <div
+              className={`${
+                project === projectNames[i + 1][0]
+                  ? "project-sub-photos-overlay"
+                  : ""
+              }`}
+              onClick={() => setProject("")}></div>
           </div>
         </>
       ))
@@ -170,13 +228,19 @@ const SubMainContent = () => {
               }`}>
               <span className="header-info text-base leading-6">About :</span>
               <span className="info-text text-base leading-6">
-              I'm Paul Choi, a software engineer and freelancer located in Orange County and Los Angeles, 
-              California, USA. With a background in developing and contributing to various applications within the Esports and Broadcasting solutions sectors, 
-              I am dedicated to crafting innovative, user-centric interfaces that elevate the overall experience.
+                I'm Paul Choi, a software engineer and freelancer located in
+                Orange County and Los Angeles, California, USA. With a
+                background in developing and contributing to various
+                applications within the Esports and Broadcasting solutions
+                sectors, I am dedicated to crafting innovative, user-centric
+                interfaces that elevate the overall experience.
               </span>
               <span className="info-text text-base leading-6">
-              I've long held a deep fascination with the entertainment and esports landscape due to its enduring challenge and ever-evolving nature. 
-              I'm open to diverse landscapes as long as they hold the potential to make a meaningful impact on the world and its users.
+                I've long held a deep fascination with the entertainment and
+                esports landscape due to its enduring challenge and
+                ever-evolving nature. I'm open to diverse landscapes as long as
+                they hold the potential to make a meaningful impact on the world
+                and its users.
               </span>
             </div>
 
@@ -187,14 +251,28 @@ const SubMainContent = () => {
               }`}>
               <span className="header-info text-base leading-6">Contact :</span>
               <span className="info-text text-base leading-6">
-              If you're interested in collaborating with me or need a well-crafted website, don't hesitate to reach out!
+                If you're interested in collaborating with me or need a
+                well-crafted website, don't hesitate to reach out!
               </span>
               <span className="info-text text-base leading-6">
-                Email: <a className="link" href="mailto:paulsoonminchoi@gmail.com?subject=Mail from Portfolio Website">paulsoonminchoi@gmail.com</a>
+                Email:{" "}
+                <a
+                  className="link"
+                  href="mailto:paulsoonminchoi@gmail.com?subject=Mail from Portfolio Website">
+                  paulsoonminchoi@gmail.com
+                </a>
               </span>
               <span className="info-text text-base leading-6">
-                Other contact info: <a className="link" href="https://www.linkedin.com/in/paulsoonminchoi/">LinkedIn</a> /
-                <a className="link" href="https://github.com/PaulSoonMinChoi">GitHub</a>
+                Other contact info:{" "}
+                <a
+                  className="link"
+                  href="https://www.linkedin.com/in/paulsoonminchoi/">
+                  LinkedIn
+                </a>{" "}
+                /
+                <a className="link" href="https://github.com/PaulSoonMinChoi">
+                  GitHub
+                </a>
               </span>
             </div>
 
@@ -203,21 +281,23 @@ const SubMainContent = () => {
               className={`info-text-container flex flex-col justify-center pr-[30px] ${
                 nav === "FAQ" ? "showNav" : ""
               }`}>
-              <span className="header-info text-base leading-6">FAQ :</span>  
+              <span className="header-info text-base leading-6">FAQ :</span>
               <span className="info-text text-base leading-6 font-bold">
                 How much experience do you have?
               </span>
               <span className="info-text text-base leading-6">
-                A: 4 years of experience as a software engineer / frontend engineer
+                A: 4 years of experience as a software engineer / frontend
+                engineer
               </span>
 
               <span className="info-text text-base leading-6 font-bold">
                 What are some of your skills?
               </span>
               <span className="info-text text-base leading-6">
-                A: React / Node.js / Typescript / Next.js / Javascript / Html & CSS / Tailwind / Docker / UI/UX Design / User Research / Agile Methodologies / Collaboration / CMS Design & Architecure
+                A: React / Node.js / Typescript / Next.js / Javascript / Html &
+                CSS / Tailwind / Docker / UI/UX Design / User Research / Agile
+                Methodologies / Collaboration / CMS Design & Architecure
               </span>
-
             </div>
 
             {/* Interests TAB */}
@@ -225,15 +305,17 @@ const SubMainContent = () => {
               className={`info-text-container flex flex-col justify-center pr-[30px] ${
                 nav === "Interests" ? "showNav" : ""
               }`}>
-              <span className="header-info text-base leading-6">Interests :</span>  
+              <span className="header-info text-base leading-6">
+                Interests :
+              </span>
               <span className="info-text text-base leading-6">
-                Lorem Ipsum has been the industry's standard dummy text ever since
-                the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not
-                only five centuries, but also the leap into electronic
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived
+                not only five centuries, but also the leap into electronic
                 typesetting, remaining essentially unchanged. It was popularised
-                in the 1960s with the release of Letraset sheets containing Lorem
-                Ipsum passages.
+                in the 1960s with the release of Letraset sheets containing
+                Lorem Ipsum passages.
               </span>
               <span className="info-text text-base leading-6">
                 I'm passionate about intrinsic, cutting-edge interfaces that
@@ -247,11 +329,17 @@ const SubMainContent = () => {
               className={`info-text-container flex flex-col justify-center pr-[30px] ${
                 nav === "Resume" ? "showNav" : ""
               }`}>
-              <span className="header-info text-base leading-6">Resume :</span>  
+              <span className="header-info text-base leading-6">Resume :</span>
               {/* <span className="info-text text-base leading-6">
                 My current resume.
               </span> */}
-              <a className='btn' href="https://drive.google.com/file/d/1mMQBAZBl0QM2-D7IW6e3vz3KP_7GuA0X/view?usp=sharing" rel="noreferrer" target="_blank">My Resume</a>
+              <a
+                className="btn"
+                href="https://drive.google.com/file/d/1mMQBAZBl0QM2-D7IW6e3vz3KP_7GuA0X/view?usp=sharing"
+                rel="noreferrer"
+                target="_blank">
+                My Resume
+              </a>
             </div>
           </div>
 
@@ -271,11 +359,11 @@ const SubMainContent = () => {
                 FAQ
               </p>
             </div>
-            <div className="navigation-sub-container flex justify-end mt-[10px]">
+            {/* <div className="navigation-sub-container flex justify-end mt-[10px]">
               <p className="navigation" onClick={() => setNav("Interests")}>
                 Interests
               </p>
-            </div>
+            </div> */}
             <div className="navigation-sub-container flex justify-end mt-[10px]">
               <p className="navigation" onClick={() => setNav("Resume")}>
                 Resume
